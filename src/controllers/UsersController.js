6d0c3ba82affae2c5d9487c0d294834557741a20
jeanjacintho <jeanjacintho@yahoo.com.br>
2024-04-17 22:34:44 -0300

    creating the user, checking if it no longer exists, updating the user, and checking whether the user's data in the database is the same as the one trying to update

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,82 @@
 const AppError = require("../utils/AppError")
+const knex = require("../database/knex")
+const { hash, compare } = require("bcryptjs");
 
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const {name, email, password} = request.body
-
-        if(!name) {
-            throw new AppError('Name is a required field´')
+    
+        try {
+            let checkUserExist
+            checkUserExist = await knex('users').select().where('email', email).first()
+    
+            if(checkUserExist) {
+                throw new AppError('This email already exists')
+            }
+    
+            const hashedPassword = await hash(password, 8)
+    
+            await knex('users').insert({
+                name,
+                email,
+                password: hashedPassword,
+            })
+    
+            response.status(201).json()
+        } catch (error) {
+            response.status(400).json({ error: error.message })
         }
-        response.status(201).json({name, email, password})
+    }
+
+    async update(request, response) {
+        const { name, email, password, old_password } = request.body;
+        const { id } = request.params;
+    
+        try {
+            const user = await knex('users').select().where('id', id).first();
+    
+            if (!user) {
+                throw new AppError('User not found');
+            }
+    
+            if (email !== user.email) {
+                const userWithUpdatedEmail = await knex('users').select().where('email', email).first();
+    
+                if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+                    throw new AppError('This email is already in use');
+                }
+            }
+    
+            user.name = name ?? user.name;
+            user.email = email ?? user.email;
+    
+            if (password && !old_password) {
+                throw new AppError('You need to enter your old password');
+            }
+    
+            if (password && old_password) {
+                const checkOldPassword = await compare(old_password, user.password);
+    
+                if (!checkOldPassword) {
+                    throw new AppError('Passwords are not the same');
+                }
+    
+                user.password = await hash(password, 8);
+            }
+    
+            await knex('users')
+                .where('id', id)
+                .update({
+                    name: user.name,
+                    email: user.email,
+                    password: user.password,
+                    updated_at: knex.fn.now()
+                });
+    
+            return response.status(200).json();
+        } catch (error) {
+            return response.status(400).json({ error: error.message });
+        }
     }
 }
 
--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -1,6 +1,6 @@
 const config = require('../../../knexfile')
 const knex = require('knex')
 
-const connection = knex.Client(config.development)
+const connection = knex(config.development)
 
 module.exports = connection
